42d76344839b9799b17fd15a57ada001e71db100
nhulm1501 <63132834+nhulm1501@users.noreply.github.com>
2020-09-28 18:34:37 +0700

    Test Suite 001

---
+++ TestSuites_001.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuites_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>85c9b4f8-051a-46f7-841a-5dcedaf04311</testSuiteGuid>
+   <testCaseLink>
+      <guid>fe68fce6-7859-4d83-8956-68c2ae02856b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>beda014c-27ce-47b1-a6c5-5e512603ae98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>984bcf94-da89-4c62-ad96-aaccf67b5207</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>17f361fe-1874-4752-a2a2-8ab040ffc6af</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a7a5afc-6351-493f-9984-4c4b82d603b1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_005</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
